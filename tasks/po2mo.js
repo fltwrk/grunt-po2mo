fa0a7132955851c1a7686d75db97b226aaf344c3
Jan T. Sott <git@idleberg.com>
2023-05-31 23:17:08 +0200

    use consistent brackets

--- tasks/po2mo.js
+++ tasks/po2mo.js
@@ -43,18 +43,38 @@ module.exports = function (grunt) {
 				if (options.alignment && !isNaN(options.alignment)) {
 					args.unshift("--alignment", options.alignment);
 				}
-				if (options.checkFormat) args.unshift("--check-format");
-				if (options.checkHeader) args.unshift("--check-header");
-				if (options.checkDomain) args.unshift("--check-domain");
-				if (options.checkCompatibility) args.unshift("--check-compatibility");
+
+				if (options.checkFormat) {
+					args.unshift("--check-format");
+				}
+
+				if (options.checkHeader) {
+					args.unshift("--check-header");
+				}
+
+				if (options.checkDomain) {
+					args.unshift("--check-domain");
+				}
+
+				if (options.checkCompatibility) {
+					args.unshift("--check-compatibility");
+				}
+
 				if (options.checkAccelerators) {
 					args.unshift("--check-accelerators", options.accelerators);
 				}
+
 				if (options.endianness) {
 					args.unshift("--endianness", options.endianness);
 				}
-				if (options.noHash) args.unshift("--no-hash");
-				if (options.useFuzzy) args.unshift("--use-fuzzy");
+
+				if (options.noHash) {
+					args.unshift("--no-hash");
+				}
+
+				if (options.useFuzzy) {
+					args.unshift("--use-fuzzy");
+				}
 
 				grunt.file.write(dest);
 				grunt.verbose.writeln("Executing:", "msgfmt", args.join(" ").trim());
